bc510b854d1bf32c6d4f22b40df7fe3264a30b19
asrawinata <55755287+asrawinata@users.noreply.github.com>
2022-06-28 18:22:47 +0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,16 +7,21 @@ public class App
 {
 
     private final String message = "Hello World!";
-    System.out.println("Current count:" + counter.getCount());
-    System.out.println("Current count:" + counter.getCount());
-    System.out.println("Current count:" + counter.getCount());
-    System.out.println("Current count:" + counter.getCount());
-    System.out.println("Current count:" + counter.getCount());
-    System.out.println("Current count:" + counter.getCount());
+    
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage());
+        
     }
 
     private final String getMessage() {
